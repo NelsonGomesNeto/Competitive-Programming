412766187dbf4bbf95fcc2a2230e96c317e1e1fa
Nelson Gomes Neto <ngn@ic.ufal.br>
2019-08-30 23:45:56 -0300

    Nice bruteforce problem, I liked my implementation

--- Online Judges/Huxley/634TecleESome.cpp
+++ Online Judges/Huxley/634TecleESome.cpp
@@ -1,90 +1,70 @@
 #include <bits/stdc++.h>
+#define lli long long int
 using namespace std;
-int s, d, r = 1;
-int connections[10][10] = {{7, 8, 9, -1}, {2, 4, 5, -1}, {1, 3, 4, 5, 6, -1}, {2, 5, 6, -1}, {1, 2, 5, 7, 8, -1}, {1, 2, 3, 4, 6, 7, 8, 9, -1}, {2, 3, 5, 8, 9, -1}, {0, 4, 5, 8, -1}, {0, 4, 5, 6, 7, 9, -1}, {0, 5, 6, 8, -1}};
-int visited[10];
 
-int getNumber(string number)
-{
-  int now = 0, pot = 1;
-  for (auto s: number)
-  {
-    now = now * pot + s - '0';
-    pot *= 10;
-  }
-  return(now);
-}
+lli s; int d;
+vector<deque<string>> ans;
+deque<string> a;
+bool visited[10];
+string digitString[10] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+vector<int> reachable[10] = {
+  {7, 8, 9}, // 0
+  {2, 4, 5}, // 1
+  {1, 3, 4, 5, 6}, // 2
+  {2, 5, 6}, // 3
+  {1, 2, 5, 7, 8}, // 4
+  {1, 2, 3, 4, 6, 7, 8, 9}, // 5
+  {2, 3, 5, 8, 9}, // 6
+  {0, 4, 5, 8}, // 7
+  {0, 4, 5, 6, 7, 9}, // 8
+  {0, 5, 6, 8} // 9
+};
 
-void bruteForce(vector<int> graph[], int u, int sum, int depth, string now, deque<string>& ans)
+void bruteForce(int u, int amount, string currentString, lli currentNumber, lli currentSum, deque<string> &current)
 {
-  // printf("%d %d %d %s %d\n", u, sum, depth, now.c_str(), getNumber(now));
-  if (visited[u]) return;
-  if (sum == s)
+  if (u == -1)
   {
-    int first = 0;
-    for (auto i: ans)
-    {
-      if (first) printf(" ");
-      printf("%s", i.c_str());
-      first = 1;
-    } printf("\n");
+    if (currentSum == s) ans.push_back(current);
+    return;
   }
-  if (depth == d && getNumber(now) + sum <= s)
-  {
-    string aux = now;
-    aux.push_back('\0');
-    ans.push_back(aux);
-    bruteForce(graph, u, sum + getNumber(now), 0, "", ans);
-    ans.pop_back();
-  }
-  visited[u] = 1;
-  for (auto v: graph[u])
-  {
-    // printf("FREAKING SHIT %d %d %d\n", u, v, depth);
-    if (depth < d)
-    {
-      string aux = now;
-      aux.push_back(v + '0');
-      bruteForce(graph, v, sum, depth + 1, aux, ans);
-    }
-    else if (v + sum + getNumber(now) == s)
-    {
-      string aux = now;
-      aux.push_back('\0');
-      ans.push_back(aux);
-      aux = "";
-      aux.push_back(v + '0');
-      aux.push_back('\0');
-      ans.push_back(aux);
-      bruteForce(graph, u, v + sum + getNumber(now), 0, "", ans);
-      ans.pop_back();
-      ans.pop_back();
-    }
-  }
-  visited[u] = 0;
+
+  visited[u] = true;
+
+  if (amount < d)
+    for (int v: reachable[u])
+      if (!visited[v])
+        bruteForce(v, amount + 1, currentString + digitString[v], currentNumber*10 + v, currentSum, current);
+
+  current.push_back(currentString);
+  bruteForce(-1, 0, "", 0, currentSum + currentNumber, current);
+
+  if (amount == d)
+    for (int v: reachable[u])
+      if (!visited[v])
+        bruteForce(v, 1, digitString[v], v, currentSum + currentNumber, current);
+
+  current.pop_back();
+
+  visited[u] = false;
 }
 
 int main()
 {
-  while (scanf("%d %d", &s, &d) && !(s == -1 && d == -1))
+  int t = 1;
+  while (scanf("%lld %d", &s, &d) && !(s == -1 && d == -1))
   {
-    vector<int> graph[10];
-    for (int i = 0, j = 0; i < 10; i ++, j = 0)
-      while (connections[i][j] != -1)
-      {
-        graph[i].push_back(connections[i][j]);
-        j ++;
-      }
-
-    printf("#%d\n", r ++);
+    ans.clear();
     for (int i = 0; i < 10; i ++)
     {
-      memset(visited, 0, sizeof(visited));
-      deque<string> ans;
-      string lol = "";
-      lol.push_back(i + '0');
-      bruteForce(graph, i, i, 1, lol, ans);
+      a.clear(), memset(visited, false, sizeof(visited));
+      bruteForce(i, 1, digitString[i], i, 0, a);
     }
+    printf("#%d\n", t ++);
+    if (ans.empty()) printf("impossivel\n");
+    else
+      for (int i = 0; i < ans.size(); i ++)
+        for (int j = 0; j < ans[i].size(); j ++)
+          printf("%s%c", ans[i][j].c_str(), j < ans[i].size() - 1 ? ' ' : '\n');
   }
   return(0);
 }
