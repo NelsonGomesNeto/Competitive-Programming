bb7ceb47bece640ba55c40ac7b22018d04310079
Nelson Gomes Neto <ngn@ic.ufal.br>
2019-10-29 15:32:37 -0300

    Optimize primeFactorization

--- Algorithms and Data Structures/Math/primeFactorization.cpp
+++ Algorithms and Data Structures/Math/primeFactorization.cpp
@@ -8,20 +8,21 @@ const int maxN = 1e6;
 bool notPrime[maxN + 1];
 vector<int> primes;
 
-list<lli> getPrimeFactors(lli num)
+vector<pair<lli, int>> getPrimeFactors(lli num)
 {
-  list<lli> primeFactors;
+  vector<pair<lli, int>> primeFactors;
   for (lli i: primes)
   {
     if (i > num) break;
     if (num % i == 0)
     {
-      primeFactors.push_back(i);
-      while (num % i == 0) num /= i;
+      int p = 0;
+      while (num % i == 0) num /= i, p ++;
+      primeFactors.push_back({i, p});
     }
   }
   if (num != 1)
-    primeFactors.push_back(num);
+    primeFactors.push_back({num, 1});
   return primeFactors;
 }
 
@@ -36,6 +37,16 @@ int main()
       notPrime[i*primes[j]] = true;
       if (i % primes[j] == 0) break;
     }
+  }
+
+  int n; scanf("%d", &n);
+  for (int i = 0; i < n; i ++)
+  {
+    lli num; scanf("%lld", &num);
+    vector<pair<lli, int>> pf = getPrimeFactors(num);
+    printf("%lld:", num);
+    for (auto &p: pf) printf(" %lld^%d", p.first, p.second);
+    printf("\n");
   }
 
   return 0;
