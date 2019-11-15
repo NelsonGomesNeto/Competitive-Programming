f85fb8a5ec393405d6ea1885ff7089d1b28fa486
Nelson Gomes Neto <ngn@ic.ufal.br>
2019-11-15 17:35:18 -0300

    Enhance SuffixArray implementation

--- Algorithms and Data Structures/String/suffixArray.cpp
+++ Algorithms and Data Structures/String/suffixArray.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
+#define DEBUG if(0)
 using namespace std;
 
-const int maxN = 1e5, maxLog = 20; int n;
+const int maxN = 1e6, maxLog = 20; int n;
 char str[maxN + 1];
 int rnk[maxN][maxLog];
 struct Node
@@ -20,6 +21,22 @@ void print()
       printf("%4d%c", rnk[i][j], j < maxLog - 1 ? ' ' : '\n');
 }
 
+int aux[maxN + 1];
+vector<Node> aux2[maxN + 1], aux3[maxN + 1];
+void countSort(Node arr[], int sz)
+{
+  for (int i = 0, end = max(26, sz); i <= end; i ++)
+    aux2[i].clear();
+  for (int i = 0; i < sz; i ++)
+    aux2[arr[i].lo].push_back(arr[i]);
+  for (int i = 0, end = max(26, sz); i <= end; i ++)
+    if (!aux2[i].empty())
+      sort(aux2[i].begin(), aux2[i].end());
+  for (int i = 0, j = 0, end = max(26, sz); i <= end; i ++)
+    for (Node &a: aux2[i])
+      arr[j ++] = a;
+}
+
 int main()
 {
   scanf("%s", str); n = strlen(str);
@@ -27,22 +44,25 @@ int main()
   for (int i = 0; i < n; i ++) rnk[i][0] = str[i] - 'a';
 
   int k;
-  for (k = 1; (1 << (k - 1)) < n; k ++)
+  for (k = 0; (1 << k) < n; k ++)
   {
     for (int i = 0; i < n; i ++)
-      nodes[i] = {rnk[i][k - 1], i + (1 << (k - 1)) < n ? rnk[i + (1 << (k - 1))][k - 1] : -1, i};
-    sort(nodes, nodes+n);
-    // sort(nodes, nodes+n); reverse(nodes, nodes+n);
+      nodes[i] = {rnk[i][k], i + (1 << k) < n ? rnk[i + (1 << k)][k] : -1, i};
+    // sort(nodes, nodes+n);
+    countSort(nodes, n);
     for (int i = 0; i < n; i ++)
-      rnk[nodes[i].index][k] = (i && nodes[i].lo == nodes[i - 1].lo && nodes[i].hi == nodes[i - 1].hi) ? rnk[nodes[i - 1].index][k] : i;
+      rnk[nodes[i].index][k + 1] = (i && nodes[i].lo == nodes[i - 1].lo && nodes[i].hi == nodes[i - 1].hi) ? rnk[nodes[i - 1].index][k + 1] : i;
   }
-  k --;
-  print();
+  DEBUG print();
   int ranking[maxN];
   for (int i = 0; i < n; i ++)
     ranking[rnk[i][k]] = i;
   for (int i = 0; i < n; i ++)
-    printf("%d %s\n", ranking[i], str+ranking[i]);
+  {
+    // DEBUG printf("%d %s\n", ranking[i], str+ranking[i]);
+    // else printf("%d\n", ranking[i]);
+    // printf("%d\n", ranking[i]);
+  }
 
   return(0);
 }
