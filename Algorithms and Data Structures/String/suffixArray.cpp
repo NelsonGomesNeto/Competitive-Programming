8f353fa185caa18ff9736cc8565cbc07e30ef7b1
Nelson Gomes Neto <ngn@ic.ufal.br>
2019-11-15 18:51:10 -0300

    Enhanced debugging of SuffixArray; finally understood ^~^

--- Algorithms and Data Structures/String/suffixArray.cpp
+++ Algorithms and Data Structures/String/suffixArray.cpp
@@ -1,24 +1,34 @@
 #include <bits/stdc++.h>
-#define DEBUG if(0)
+#define DEBUG if(1)
 using namespace std;
 
 const int maxN = 1e6, maxLog = 20; int n;
-char str[maxN + 1];
+char s[maxN + 1];
 int rnk[maxN][maxLog];
 struct Node
 {
   int lo, hi, index;
-  bool operator<(const Node &no) const
+  bool operator<(const Node &other) const
   {
-    return(lo < no.lo || (lo == no.lo && hi < no.hi));
+    return lo < other.lo || (lo == other.lo && hi < other.hi);
+  }
+  bool operator==(const Node &other) const
+  {
+    return lo == other.lo && hi == other.hi;
   }
 };
 Node nodes[maxN];
 void print()
 {
   for (int i = 0; i < n; i ++)
+    printf("(%d %d %d)%c", nodes[i].lo, nodes[i].hi, nodes[i].index, i < n - 1 ? ' ' : '\n');
+  printf("       "); for (int i = 0; i < maxLog; i ++) printf("%4d%c", i, i < maxLog - 1 ? ' ' : '\n');
+  for (int i = 0; i < n; i ++)
+  {
+    printf("%4d - ", i);
     for (int j = 0; j < maxLog; j ++)
       printf("%4d%c", rnk[i][j], j < maxLog - 1 ? ' ' : '\n');
+  }
 }
 
 int aux[maxN + 1];
@@ -39,27 +49,39 @@ void countSort(Node arr[], int sz)
 
 int main()
 {
-  scanf("%s", str); n = strlen(str);
+  scanf("%s", s); n = strlen(s);
 
-  for (int i = 0; i < n; i ++) rnk[i][0] = str[i] - 'a';
+  DEBUG {
+    for (int i = 0; i < n; i ++)
+      printf("%4d%c", i, i < n - 1 ? ' ' : '\n');
+    for (int i = 0; i < n; i ++)
+      printf("   %c%c", s[i], i < n - 1 ? ' ' : '\n');
+  }
+
+  for (int i = 0; i < n; i ++)
+    rnk[i][0] = s[i];
 
   int k;
+  DEBUG print();
   for (k = 0; (1 << k) < n; k ++)
   {
     for (int i = 0; i < n; i ++)
       nodes[i] = {rnk[i][k], i + (1 << k) < n ? rnk[i + (1 << k)][k] : -1, i};
-    // sort(nodes, nodes+n);
-    countSort(nodes, n);
+    sort(nodes, nodes+n);
+    // countSort(nodes, n);
     for (int i = 0; i < n; i ++)
-      rnk[nodes[i].index][k + 1] = (i && nodes[i].lo == nodes[i - 1].lo && nodes[i].hi == nodes[i - 1].hi) ? rnk[nodes[i - 1].index][k + 1] : i;
+      rnk[nodes[i].index][k + 1] = (i && nodes[i] == nodes[i - 1]) ? rnk[nodes[i - 1].index][k + 1] : i;
+    DEBUG {
+      printf("%d ------------------------\n", k);
+      print();
+    }
   }
-  DEBUG print();
   int ranking[maxN];
   for (int i = 0; i < n; i ++)
     ranking[rnk[i][k]] = i;
   for (int i = 0; i < n; i ++)
   {
-    // DEBUG printf("%d %s\n", ranking[i], str+ranking[i]);
+    DEBUG printf("%d %s\n", ranking[i], s+ranking[i]);
     // else printf("%d\n", ranking[i]);
     // printf("%d\n", ranking[i]);
   }
